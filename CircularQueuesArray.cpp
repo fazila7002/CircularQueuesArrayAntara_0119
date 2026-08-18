5b7d06ca90ccba70d3354982a8876e85635900a2
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-18 15:05:58 +0700

    jika posisi front itu -1 maka ini bisa di katakan posisi kosong  akan di tampilan antrian kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -74,4 +74,15 @@ class Queues
         int FRONT_Position = FRONT;
         int REAR_Position = REAR;
 
+        if (FRONT_Position == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+ 
+    }
+
+
+}
+
     
