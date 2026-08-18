00808850a9848a15e0f30f6868f878e9c56e49e8
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-18 15:10:41 +0700

    ini untuk switch casenya

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -124,6 +124,40 @@ int main()
         cout << "4. Exit " << endl;
         cin >> ch;
         cout << endl;
+
+           switch (ch)
+        {
+        case '1':
+        {
+            q.insert();
+            break;
+        }
+        case '2':
+        {
+            q.remove();
+            break;
+        }
+        case '3':
+        {
+            q.display();
+            break;
+
+        }
+        case '4':
+        {
+            return 0;
+        }
+        default:
+        {
+            cout << "Invalid opstion !!" << endl;
+            break;
+        }
+        }
+        }
+
+}
+
+    
  
     
 
